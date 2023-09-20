a578a435c5195cc0613e88d4acc687d9cb42c2fa
EmsR434 <142956103+EmsR434@users.noreply.github.com>
2023-09-20 00:04:20 -0700

    Update checklist.py

--- checklist.py
+++ checklist.py
@@ -14,14 +14,11 @@ def destroy(index):
     checklist.pop(index)
 def test():
     create("purple sox")
-create("red cloak")
-
-print(read(0))
-print(read(1))
-
-update(0, "purple socks")
-destroy(1)
-
-print(read(0))
-print(read(1))
+ create("red cloak")
+ print(read(0))
+ print(read(1))
+ update(0, "purple socks")
+ destroy(1)
+ print(read(0))
+ print(read(1))
 
