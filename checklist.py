fd3787a5b6fc5d10c43dce4fe379395fd3608b1d
EmsR434 <142956103+EmsR434@users.noreply.github.com>
2023-09-25 17:30:54 -0700

    Update checklist.py

--- checklist.py
+++ checklist.py
@@ -12,13 +12,60 @@ def update(index, item):
 # DESTROY
 def destroy(index):
     checklist.pop(index)
+# LISTING
+def list_all_items():
+    index = 0
+    for list_item in checklist:
+        print(list_item)
+        index += 1
+
+# def mark_completed(index):
+    for list_item in checklist:
+        print('√purple sox')
+
+def select(function_code):
+    if function_code == "C":
+        input_item = user_input("Input item:")
+        create(input_item)
+
+    elif function_code == "R":
+        item_index = user_input("Index Number?")
+
+        # Remember that item_index must actually exist or our program will crash.
+        read(item_index)
+
+    # Print all items
+    elif function_code == "P":
+        list_all_items()
+
+    # Catch all
+    else:
+        print("Unknown Option")
+        
+def user_input(prompt):
+    # the input function will display a message in the terminal
+    # and wait for user input.
+    user_input = input(prompt)
+    return user_input
+
+
 def test():
     create("purple sox")
- create("red cloak")
- print(read(0))
- print(read(1))
- update(0, "purple socks")
- destroy(1)
- print(read(0))
- print(read(1))
+    create("red cloak")
 
+    print(read(0))
+    print(read(1))
+
+    update(0, "purple sox")
+
+    destroy(1)
+
+    print(read(0))
+
+    list_all_items()
+
+    select("R")
+
+    list_all_items()
+
+test()
